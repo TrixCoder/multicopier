8e354e5fa939d7dd95163954036021e5e117235b
Utkarsh Kumar <utk.bkkg@gmail.com>
2023-11-26 14:17:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import keyboard
+import pyperclip
+
+copied_texts = {}
+
+def on_key_event(e):
+    if e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('c') and keyboard.is_pressed('1'):
+        copied_texts['1'] = pyperclip.paste()
+        print('Text copied to 1:', copied_texts['1'])
+
+    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('c') and keyboard.is_pressed('2'):
+        copied_texts['2'] = pyperclip.paste()
+        print('Text copied to 2:', copied_texts['2'])
+
+    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('v') and keyboard.is_pressed('1'):
+        pyperclip.copy(copied_texts.get('1', ''))
+        keyboard.press_and_release('ctrl+v')
+
+    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('v') and keyboard.is_pressed('2'):
+        pyperclip.copy(copied_texts.get('2', ''))
+        keyboard.press_and_release('ctrl+v')
+
+keyboard.hook(on_key_event)
+
+keyboard.wait()
+
