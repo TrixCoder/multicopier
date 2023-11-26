12c4ddcfb28a0b74d66b95c4c673f686d6482bb6
Trixter <utk.bkkg@gmail.com>
2023-11-26 15:15:19 +0530

    fixed multi-digit support

--- main.py
+++ main.py
@@ -4,23 +4,16 @@ import pyperclip
 copied_texts = {}
 
 def on_key_event(e):
-    if e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('c') and keyboard.is_pressed('1'):
-        copied_texts['1'] = pyperclip.paste()
-        print('Text copied to 1:', copied_texts['1'])
+    if e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('c') and e.name.isdigit():
+        slot_key = e.name
+        copied_texts[slot_key] = pyperclip.paste()
+        print(f'Text copied to Slot {slot_key}:', copied_texts[slot_key])
 
-    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('c') and keyboard.is_pressed('2'):
-        copied_texts['2'] = pyperclip.paste()
-        print('Text copied to 2:', copied_texts['2'])
-
-    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('b') and keyboard.is_pressed('1'):
-        pyperclip.copy(copied_texts.get('1', ''))
-        keyboard.press_and_release('ctrl+v')
-
-    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('b') and keyboard.is_pressed('2'):
-        pyperclip.copy(copied_texts.get('2', ''))
+    elif e.event_type == keyboard.KEY_DOWN and keyboard.is_pressed('ctrl') and keyboard.is_pressed('b') and e.name.isdigit():
+        slot_key = e.name
+        pyperclip.copy(copied_texts.get(slot_key, ''))
         keyboard.press_and_release('ctrl+v')
 
 keyboard.hook(on_key_event)
 
 keyboard.wait()
-
